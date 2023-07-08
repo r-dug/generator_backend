8b7582f714684b99b4df55db123cd2fa4d08fd5b
Richard Douglas <r.douglas.42092@gmail.com>
2023-07-08 11:49:52 -0700

    Persistent login. hope it works on Heroku.

--- server.js
+++ server.js
@@ -233,7 +233,7 @@ app.post('/login', async (req, res) => {
                 sameSite: 'none', // cross-site
                 secure: true, // Set to true if using HTTPS
                 httpOnly: false, // Prevent client-side JavaScript from accessing cookies
-                maxAge: 1000*60*30, // Session expiration time (in milliseconds)
+                maxAge: 1000*60*30*1000, // Session expiration time (in milliseconds)
                 domain: process.env.COOKIE_ALLOW,
                 path: "/"
             })
