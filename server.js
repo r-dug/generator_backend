362fc1d87b9066ed3c1efcbcd61f3ef0b88ec829
Richard Douglas <r.douglas.42092@gmail.com>
2023-07-08 16:07:26 -0700

    Persistent login. hope it works on Heroku.

--- server.js
+++ server.js
@@ -82,6 +82,7 @@ const app = express()
             saveUninitialized: false, // Do not save uninitialized sessions
             unset: 'destroy',
             cookie: {
+                proxy: true,
                 sameSite: 'none', // cross-site
                 secure: true, // Set to true if using HTTPS
                 httpOnly: false, // Prevent client-side JavaScript from accessing cookies
@@ -228,15 +229,15 @@ app.post('/login', async (req, res) => {
             const expires = req.session.cookie.expires
             res.header('Access-Control-Allow-Origin', FRONT_END);
             res.header('Access-Control-Allow-Credentials', 'true');
-            // res.cookie("session", existingUser._id.toString(), {
-            //     proxy: true,
-            //     sameSite: 'none', // cross-site
-            //     secure: true, // Set to true if using HTTPS
-            //     httpOnly: false, // Prevent client-side JavaScript from accessing cookies
-            //     maxAge: 60*30*1000, // Session expiration time (in milliseconds)
-            //     domain: process.env.COOKIE_ALLOW,
-            //     path: "/"
-            // })
+            res.cookie("session", existingUser._id.toString(), {
+                proxy: true,
+                sameSite: 'none', // cross-site
+                secure: true, // Set to true if using HTTPS
+                httpOnly: false, // Prevent client-side JavaScript from accessing cookies
+                maxAge: 60*30*1000, // Session expiration time (in milliseconds)
+                domain: process.env.COOKIE_ALLOW,
+                path: "/"
+            })
             return res.status(200).json({
                 message: "Login Successful"
             })
